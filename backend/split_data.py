90d7aca71687267646312c46c593413642c58a2b
Radha Goel <rgoel5934@gmail.com>
2026-03-24 01:58:55 +0530

    Implemented Text Splitting

---
+++ backend/split_data.py
@@ -0,0 +1,15 @@
+#1. Load text from file
+from load_data import file_content
+
+#2. import text splitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+#3. create an instance of the text splitter
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
+
+#4. split the text into chunks
+chunks = text_splitter.split_text(file_content)
+
+#5. print the chunks
+for i, chunk in enumerate(chunks):
+    print(f"Chunk {i+1}:\n{chunk}\n")   
